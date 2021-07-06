23bf038103846a72dccb86b07bb22e8f781598a8
MrDebugCode <hilarior257@gmail.com>
2021-07-05 23:39:16 -0600

    Aprendiendo sobre la creacion de componentes 'funcionales' y usando 'clases', tambien sobre la configuracion inicial de un proyecto con react

---
+++ src/components/Badge.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+class Badge extends React.Component{
+    constructor(){
+        super()
+
+        const METHODS = [
+            'agregar',
+            'quitar',
+            'limpiar'
+        ]
+
+        METHODS.forEach((method) => {
+            this[method] = this[method].bind(this)
+        })
+
+        this.state = {
+            cantidad: 0
+        }
+    }
+
+    agregar(){
+        this.setState({
+            cantidad: this.state.cantidad + 1
+        })
+    }
+    quitar(){
+        this.setState({
+            cantidad: this.state.cantidad - 1
+        })
+    }
+    limpiar(){
+        this.setState({
+            cantidad: 0
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>{this.props.name}</h3>
+                <hr/>
+                <div>Cantidad : {this.state.cantidad}</div>
+                <button onClick={this.agregar.bind(this)}>+</button>
+                <button onClick={this.limpiar.bind(this)}>Limpiar</button>
+                <button onClick={this.quitar.bind(this)}>-</button>
+                <p>{this.props.price}</p>
+            </div>
+        );
+    }
+}
+
+export default Badge;
